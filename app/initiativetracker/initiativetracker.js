fd957dca435ab68a3f97be0005845ca2668c1a19
Hana Lee <leehana@stanford.edu>
2017-07-06 16:30:54 -0700

    Update initiativetracker.js

--- app/initiativetracker/initiativetracker.js
+++ app/initiativetracker/initiativetracker.js
@@ -60,7 +60,7 @@ myApp.config(['$routeProvider', function($routeProvider) {
 	//Add player to initiative order
 	$scope.addPlayerToInitiative = function(player) {
 		var initiative = Math.floor(Math.random()*(20)) + 1 + parseFloat(player.mod);		// generate random initiative 1-20
-		$scope.initiativeOrder.push({type: 'player', name: player.name, original: initiative, effective: initiative});
+		$scope.initiativeOrder.push({type: 'player', name: player.name, original: initiative, effective: initiative, mod: parseFloat(player.mod)});
 		sortInitiativeOrder();
 	};
 	//Add all players to initiative order
@@ -72,7 +72,7 @@ myApp.config(['$routeProvider', function($routeProvider) {
 	//Add monster to initiative order
 	$scope.addMonsterToInitiative = function(monster, initiative) {
 		initiative = parseFloat(initiative) + parseFloat(monster.mod);
-		$scope.initiativeOrder.push({type: 'monster', name: monster.name, original: initiative, effective: initiative, fullHealth: monster.health, health: monster.health});
+		$scope.initiativeOrder.push({type: 'monster', name: monster.name, original: initiative, effective: initiative, fullHealth: monster.health, health: monster.health, mod: parseFloat(monster.mod)});
 		$scope.inputs.initiatives[monster.name] = '';
 		$scope.removeMonster(monster);
 		sortInitiativeOrder();
@@ -112,89 +112,13 @@ myApp.config(['$routeProvider', function($routeProvider) {
 		monster.health = monster.health - monster.damage;
 		monster.damage = 0;
 	}
-
+	//Sort initiative order
 	function sortInitiativeOrder() {
 		$scope.initiativeOrder.sort(function(a, b) {
+			if (a.effective === b.effective) {
+				return b.mod - a.mod;
+			}
 			return b.effective - a.effective;
 		});
 	}
-	// $scope.players = [{name: "Hana", init: 17, health: 20, damage: 0, status: []}];
-	// $scope.numTurns = 1;
-	// $scope.currentPlayerIndex = 0;
-
-	// $scope.addPlayer = function() {
-	// 	var currentlyActivePlayer = $scope.players[$scope.currentPlayerIndex];
-	// 	var player = {name: $scope.name, init: $scope.init, health: $scope.health, damage: 0, status: []};
-	// 	$scope.players.push(player);
-	// 	$scope.name = "";
-	// 	$scope.init = "";
-	// 	$scope.health = "";
-	// 	$scope.players.sort(function(a,b) {
-	// 		return b.init - a.init;
-	// 	});
-	// 	// if the new player was added above the active player, keep the current active player
-	// 	if (currentlyActivePlayer !== $scope.players[$scope.currentPlayerIndex]) {
-	// 		$scope.currentPlayerIndex = ($scope.currentPlayerIndex+1) % $scope.players.length;
-	// 	}
-	// };
-
-	// $scope.killPlayer = function(player) {
-	// 	var currentlyActivePlayer = $scope.players[$scope.currentPlayerIndex];
-	// 	$scope.players = $scope.players.filter(function(x) {
-	// 		return x.name !== player.name;
-	// 	});
-	// 	if ($scope.players.length === 0) {
-	// 		// if there are no more players
-	// 		$scope.currentPlayerIndex = 0;
-	// 	} else if (currentlyActivePlayer.name === player.name) {
-	// 		// if it was the dead player's turn, increment by one
-	// 		$scope.currentPlayerIndex = ($scope.currentPlayerIndex+1) % $scope.players.length;
-	// 	} else if (currentlyActivePlayer !== $scope.players[$scope.currentPlayerIndex]) {
-	// 		// if the dead player went before the active player, keep the current active player
-	// 		$scope.currentPlayerIndex = ($scope.currentPlayerIndex-1) % $scope.players.length;
-	// 	}
-	// };
-
-	// $scope.damagePlayer = function(player) {
-	// 	player.health = player.health - player.damage;
-	// 	player.damage = 0;
-	// };
-
-	// $scope.addStatusCondition = function(player) {
-	// 	player.status.push({condition: player.newCondition, turns: player.newConditionTurns});
-	// 	player.newCondition = "";
-	// 	player.newConditionTurns = "";
-	// 	player.showNewCondition = false;
-	// };
-
-	// $scope.removeCondition = function(player, condition) {
-	// 	player.status.splice(player.status.indexOf(condition),1);
-	// }
-
-	// $scope.advanceTurn = function() {
-	// 	var currentPlayerIndex = $scope.currentPlayerIndex;
-	// 	$scope.currentPlayerIndex = ($scope.currentPlayerIndex+1) % $scope.players.length;
-	// 	if ($scope.currentPlayerIndex <= currentPlayerIndex) {
-	// 		$scope.numTurns = $scope.numTurns + 1;
-	// 	}
-	// 	var player = $scope.players[currentPlayerIndex];
-	// 	for (var i = player.status.length - 1; i >= 0; i--) {
-	// 		player.status[i]['turns']--;
-	// 		if (player.status[i]['turns'] <= 0) {
-	// 			player.status.splice(i,1);
-	// 		}
-	// 	}
-
-	// 	// subtract 1 from each condition's turn count
-	// };
-
-	// $scope.resetTurns = function() {
-	// 	$scope.numTurns = 1;
-	// }
-
-	// $scope.newBattle = function() {
-	// 	$scope.players = [];
-	// 	$scope.numTurns = 1;
-	// 	$scope.currentPlayerIndex = 0;
-	// }
 }]);
